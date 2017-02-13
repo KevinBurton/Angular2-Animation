55998f03322434db924f0f2f946cc05f230c4a69
Kevin Burton <rkevinburton@charter.net>
2017-02-12 20:30:57 -0600

    Multiple triggers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
 
 @Component({
   selector: 'my-app',
-  template: `<button (click)='toggleState()'>My Button</button>
+  template: `<button (click)='toggleState()' [@removeMe]='btnState'>My Button</button>
   <ul>
-    <li *ngFor="let item of items" [@myTrigger]='state'>{{ item }}</li>
+    <li *ngFor="let item of items"
+        [@myTrigger]='state'
+        (@myTrigger.start)="animStart($event)"
+        (@myTrigger.done)="animDone($event)">{{ item }}</li>
   </ul>
   `,
   styles: [`
@@ -36,15 +39,38 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
           style({opacity:1, transform: 'translateY(0px)', offset: 1})
         ]))
       ])
+    ]),
+    trigger('removeMe', [
+      state('out', style({
+        transform:'scale(0)',
+        opacity: 0
+      })),
+      transition('* => out', [
+        animate('500ms 0s ease-in', keyframes([
+          style({opacity: 1, transform: 'translateX(-8px)', offset: 0}),
+          style({opacity: 1, transform: 'translateX(0px)', offset: 0.3}),
+          style({opacity: 0, transform: 'translateX(50px)', offset: 1})
+        ]))
+      ])
     ])
   ]
 })
 export class AppComponent  {
   state: string = 'small';
-  items: string[] = [];
+  btnState: string ='in';
+  items: string[] = new Array<string>();
   toggleState() {
     //this.state = (this.state === 'small' ? 'large' : 'small');
     this.items.push("Another item");
     this.state = "fadeIn";
+    this.btnState ="out";
+  }
+  animStart(event:any){
+    console.log("Animation started");
+    console.log(event);
+  }
+  animDone(event:any){
+    console.log("Animation finished");
+    console.log(event);
   }
 }
